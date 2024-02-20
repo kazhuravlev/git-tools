6916a6cd51fd281dde9972e343ad5d774dd492b1
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-02-20 16:28:02 +0100

    integrate new version of cli pkg

--- cmd/gt/helpers.go
+++ cmd/gt/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
@@ -8,8 +9,8 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
-func withManager(action func(c *cli.Context, manager *repomanager.Manager) error) cli.ActionFunc {
-	return func(c *cli.Context) error {
+func withManager(action func(context.Context, *cli.Command, *repomanager.Manager) error) cli.ActionFunc {
+	return func(ctx context.Context, c *cli.Command) error {
 		repoPath := c.String(flagRepoPath)
 		if repoPath == "" {
 			return errors.New("path to repo must be set by flag " + flagRepoPath)
@@ -20,6 +21,6 @@ func withManager(action func(c *cli.Context, manager *repomanager.Manager) error
 			return fmt.Errorf("cannot build repo manager: %w", err)
 		}
 
-		return action(c, manager)
+		return action(ctx, c, manager)
 	}
 }
